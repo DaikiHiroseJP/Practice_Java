7966e18d199f9a7fb40fd6bb3347dc597b74d74c
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2023-01-21 09:25:56 +0000

    Commit

---
+++ test2_5/Test5.java
@@ -0,0 +1,17 @@
+package test2_5;
+
+class Test5 {
+  public static void main(String[] args) {
+    int number = 12;
+
+    // numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
+    if (number < 10) {
+      System.out.println("10より小さい");
+    }else if (number < 20) {
+      System.out.println("10以上、20より小さい");
+    }else{
+      System.out.println("どちらでもない");
+    }
+
+  }
+}
