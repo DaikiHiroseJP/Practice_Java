3a5f90e6569944c9d8d07c11354597347b240e85
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2023-01-22 03:22:45 +0000

    Commit

---
+++ test2_7/Test7.java
@@ -0,0 +1,10 @@
+package test2_7;
+
+class Test7 {
+  public static void main(String[] args) {
+    // for文を用いて、繰り返し処理をつくってください
+    for (int i = 1;i <= 10;i ++){
+    System.out.println(i+"回目のループです");
+    }
+  }
+}
