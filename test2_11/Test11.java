b7428bacc2c36f466524519d6771208b27c66a59
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2023-01-22 03:56:32 +0000

    Commit

---
+++ test2_11/Test11.java
@@ -0,0 +1,12 @@
+package test2_11;
+
+class Test11 {
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for (int i = 0;i < names.length; i++){
+    System.out.println("私の名前は"+names[i]+"です");
+    }
+  }
+}
