6211b5ea78f4394b2f6b8fbbb5c0e5f252a2ee6d
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2023-01-24 06:45:54 +0000

    Commit

---
+++ test3_1/Pack.java
@@ -0,0 +1,7 @@
+package test3_1;
+
+public class Pack {
+	void print(){
+		System.out.println("ファイル分割確認");
+	}
+}
---
+++ test3_1/Test.java
@@ -0,0 +1,8 @@
+package test3_1;
+
+public class Test {
+	public static void main(String[] args){
+		Pack p = new Pack();
+		p.print();
+	}
+}
